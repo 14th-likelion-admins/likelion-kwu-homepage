60e2fe0f06d031aba0bacb1d1b04b2fef67c01ee
Eine0322 <eine113589@gmail.com>
2026-01-05 22:08:02 +0900

    feat : 패키지파일 추가

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html', // 루트 HTML
+    './src/**/*.{js,jsx,ts,tsx}', // src 폴더 안 모든 JS/JSX/TS/TSX
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
